c4382bd9ec97471803d23a6035398895f9556866
Nikhil Chhabra <91406971+procrastinatedbread@users.noreply.github.com>
2023-06-23 04:33:46 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+
+<div >
+<h3>1 month Free</h3>
+<h1>Individual Plan</h1>
+<h4>$9.99</h4>
+<hr/>
+<ul>
+<li>Ad-free music listening</li>
+<li>Download 10k songs/device</li>
+<li>Login from 3 devices</li>
+</ul>
+<button>Sign up</button>
+  
+</div>
+`;
